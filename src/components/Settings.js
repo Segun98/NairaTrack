f46474a9f4fc1ab3ef151b47187447ece525f8cc
Segun <shegunolanitori@gmail.com>
2020-03-16 15:18:04 +0100

    New settings page under construction

---
+++ src/components/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Settings() {
+    return (
+        <div>
+            <h2 style={{textAlign:"center", paddingTop:"30px"}}>Under construction</h2>
+            <ul>
+              <h3> You would now be able to </h3>
+               <li>- Add your name</li>
+               <li>- Name your accounts</li>
+            </ul>
+        </div>
+    )
+}

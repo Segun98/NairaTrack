8f836c5ae6ce3b0af00e8b67920b2665cde35d87
Segun <shegunolanitori@gmail.com>
2020-03-18 16:03:34 +0100

    Moved delete accounts feature to settings

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,71 +1,34 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { AccountOneContext } from "../Contexts/AccountOne";
 import { AccountTwoContext } from "../Contexts/AccountTwo";
 import { Link } from "react-router-dom";
 import Commas from "../Commas";
 
 export default function Index() {
-  const {
-    incomeOneTotal,
-    ExpensesOneTotal,
-    AccountOneBalance,
-    valueOneAccountOne,
-    valueTwoAccountOne
-  } = useContext(AccountOneContext);
-  const {
-    incomeTwoTotal,
-    ExpensesTwoTotal,
-    AccountTwoBalance,
-    valueOneAccountTwo,
-    valueTwoAccountTwo
-  } = useContext(AccountTwoContext);
-
-  const [IncomeOne, setIncomeOne] = valueOneAccountOne;
-  const [ExpensesOne, setExpensesOne] = valueTwoAccountOne;
-
-  const [IncomeTwo, setIncomeTwo] = valueOneAccountTwo;
-  const [ExpensesTwo, setExpensesTwo] = valueTwoAccountTwo;
+  const { incomeOneTotal, ExpensesOneTotal, AccountOneBalance } = useContext(
+    AccountOneContext
+  );
+  const { incomeTwoTotal, ExpensesTwoTotal, AccountTwoBalance } = useContext(
+    AccountTwoContext
+  );
 
   // const LinkStyle = {
   //   textDecoration: "none",
   //   color: "whitesmoke"
   // };
 
-  const [Modal, setModal] = useState(true);
-  const [ModalTwo, setModalTwo] = useState(true);
-
-  function handleDelete() {
-    const newAccountOne = [...IncomeOne, ...ExpensesOne];
-    newAccountOne.splice(0, newAccountOne.length);
-    setIncomeOne(newAccountOne);
-    setExpensesOne(newAccountOne);
-    setModal(true);
-  }
-  function deleteItem() {
-    setModal(false);
-  }
-
-  function handleDeleteTwo() {
-    const newAccountTwo = [...IncomeTwo, ...ExpensesTwo];
-    newAccountTwo.splice(0, newAccountTwo.length);
-    setIncomeTwo(newAccountTwo);
-    setExpensesTwo(newAccountTwo);
-    setModalTwo(true);
-  }
-  function deleteItemTwo() {
-    setModalTwo(false);
-  }
   // Quotes
   const quotes = [
     "It is important to track every of your daily transactions, start accounting and see the difference! Remember to share this app with someone",
     "Accounting is the language of business - Warren Buffett",
     "Do not save what is left after spending; instead spend what is left after saving - Warren Buffett",
     "The principles of wealth are true regarding large amounts and small amounts. It all begins with the smallest unit of currency - Hendrith Smith",
-    "Look everywhere you can to cut a little bit from your expenses. It will all add up to a meaningful sum - Suze Orman"
+    "Look everywhere you can to cut a little bit from your expenses. It will all add up to a meaningful sum - Suze Orman",
+    "It is adviced to track your business expenses separately from your personal, eventhough initially it may feel like they are one in the same if you are a one-man shop"
   ];
 
   // 2. Generate random number between 0 and quotes' length
-  const random = Math.floor(Math.random() * (quotes.length - 1 - 0 + 1)) + 0;
+  const random = Math.floor(Math.random() * ((quotes.length - 1) - 0 + 1)) + 0;
 
   //3. display quotes based on the generated random number
   const displayQuotes = quotes[random];
@@ -76,56 +39,6 @@ export default function Index() {
           <h4>"{displayQuotes}"</h4>
         </div>
       </section>
-
-      <div className={Modal ? "modal" : "modal close-modal"}>
-        <div className="modal-content">
-          <div className="modal-head">
-            <p>Delete Account</p>
-            <p
-              onClick={() => {
-                setModal(true);
-              }}
-            >
-              &times;
-            </p>
-          </div>
-          <div className="modal-body">
-            <h4>
-              Are you sure you want to delete PERSONAL ACCOUNT? All data will be
-              lost and cannot be recovered.
-            </h4>
-          </div>
-          <div className="modal-footer">
-            <div></div>
-            <button onClick={handleDelete}>Delete</button>
-          </div>
-        </div>
-      </div>
-
-      <div className={ModalTwo ? "modal-two" : "modal-two close-modal"}>
-        <div className="modal-content">
-          <div className="modal-head">
-            <p>Delete Account</p>
-            <p
-              onClick={() => {
-                setModalTwo(true);
-              }}
-            >
-              &times;
-            </p>
-          </div>
-          <div className="modal-body">
-            <h4>
-              Are you sure you want to delete BUSINESS ACCOUNT? All data will be
-              lost and cannot be recovered.
-            </h4>
-          </div>
-          <div className="modal-footer">
-            <div></div>
-            <button onClick={handleDeleteTwo}>Delete</button>
-          </div>
-        </div>
-      </div>
 
       <section className="index-account-one-two-wrap">
         <section className="index-account-one">
@@ -154,25 +67,6 @@ export default function Index() {
               Manage
             </Link>
           </button>
-          <button
-            style={{
-              border: "none",
-              color: "white",
-              background: "#B2BEC3",
-              marginLeft: "5px",
-              cursor: "pointer",
-              display:
-                ExpensesOneTotal === 0 &&
-                incomeOneTotal === 0 &&
-                AccountOneBalance === 0
-                  ? "none"
-                  : "inline-block"
-            }}
-            title="Delete"
-            onClick={deleteItem}
-          >
-            Clear
-          </button>
         </section>
 
         <section className="index-account-two">
@@ -200,25 +94,6 @@ export default function Index() {
             >
               Manage
             </Link>
-          </button>
-          <button
-            style={{
-              border: "none",
-              color: "white",
-              background: "#B2BEC3",
-              marginLeft: "5px",
-              cursor: "pointer",
-              display:
-                ExpensesTwoTotal === 0 &&
-                incomeTwoTotal === 0 &&
-                AccountTwoBalance === 0
-                  ? "none"
-                  : "inline-block"
-            }}
-            title="Delete"
-            onClick={deleteItemTwo}
-          >
-            Clear
           </button>
         </section>
       </section>
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,13 +1,105 @@
 import React, { useContext, useState } from "react";
 import { AccountOneContext } from "../Contexts/AccountOne";
+import { AccountTwoContext } from "../Contexts/AccountTwo";
 
 export default function Settings() {
   const [alertName, setalertName] = useState(false);
   //    Name State
-  const { inputName, setinputName, setName } = useContext(AccountOneContext);
+  const {
+    inputName,
+    setinputName,
+    setName,
+    valueOneAccountOne,
+    valueTwoAccountOne
+  } = useContext(AccountOneContext);
+
+  const { valueOneAccountTwo, valueTwoAccountTwo } = useContext(
+    AccountTwoContext
+  );
+
+  const [IncomeOne, setIncomeOne] = valueOneAccountOne;
+  const [ExpensesOne, setExpensesOne] = valueTwoAccountOne;
+
+  const [IncomeTwo, setIncomeTwo] = valueOneAccountTwo;
+  const [ExpensesTwo, setExpensesTwo] = valueTwoAccountTwo;
+
+  const [Modal, setModal] = useState(true);
+  const [ModalTwo, setModalTwo] = useState(true);
+
+  function handleDelete() {
+    const newAccountOne = [...IncomeOne, ...ExpensesOne];
+    newAccountOne.splice(0, newAccountOne.length);
+    setIncomeOne(newAccountOne);
+    setExpensesOne(newAccountOne);
+    setModal(true);
+  }
+  function deleteItem() {
+    setModal(false);
+  }
+
+  function handleDeleteTwo() {
+    const newAccountTwo = [...IncomeTwo, ...ExpensesTwo];
+    newAccountTwo.splice(0, newAccountTwo.length);
+    setIncomeTwo(newAccountTwo);
+    setExpensesTwo(newAccountTwo);
+    setModalTwo(true);
+  }
+  function deleteItemTwo() {
+    setModalTwo(false);
+  }
 
   return (
     <div className="settings-page">
+      <div className={Modal ? "modal" : "modal close-modal"}>
+        <div className="modal-content">
+          <div className="modal-head">
+            <p>Delete Account</p>
+            <p
+              onClick={() => {
+                setModal(true);
+              }}
+            >
+              &times;
+            </p>
+          </div>
+          <div className="modal-body">
+            <h4>
+              Are you sure you want to delete PERSONAL ACCOUNT? All data will be
+              lost and cannot be recovered.
+            </h4>
+          </div>
+          <div className="modal-footer">
+            <div></div>
+            <button onClick={handleDelete}>Delete</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={ModalTwo ? "modal-two" : "modal-two close-modal"}>
+        <div className="modal-content">
+          <div className="modal-head">
+            <p>Delete Account</p>
+            <p
+              onClick={() => {
+                setModalTwo(true);
+              }}
+            >
+              &times;
+            </p>
+          </div>
+          <div className="modal-body">
+            <h4>
+              Are you sure you want to delete BUSINESS ACCOUNT? All data will be
+              lost and cannot be recovered.
+            </h4>
+          </div>
+          <div className="modal-footer">
+            <div></div>
+            <button onClick={handleDeleteTwo}>Delete</button>
+          </div>
+        </div>
+      </div>
+
       <section className="change-name">
         <div>
           <h4 style={{ textAlign: "center", marginBottom: "40px" }}>
@@ -18,7 +110,7 @@ export default function Settings() {
           onSubmit={e => {
             e.preventDefault();
             setName(inputName);
-            setalertName(true)
+            setalertName(true);
           }}
         >
           <input
@@ -45,9 +137,56 @@ export default function Settings() {
           Name changed successfuly!
         </div>
       </section>
-      {/* <section className="change-personal"></section>
-      <section className="change-business"></section>
-      <section className="delete-account"></section> */}
+      <section className="delete-accounts">
+        <div>
+          <h4 style={{ textAlign: "center", marginBottom: "20px" }}>
+            Delete Accounts{" "}
+          </h4>
+        </div>
+        <div className="delete-accounts-wrap">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h3>Personal Account</h3>
+            <p>Delete all transactions data from Personal Account</p>
+          </div>
+          <button
+            style={{
+              border: "none",
+              color: "white",
+              background: "red",
+              marginLeft: "5px",
+              cursor: "pointer",
+              fontSize: "14px"
+            }}
+            title="Delete"
+            onClick={deleteItem}
+          >
+            Delete
+          </button>
+        </div>
+      </section>
+
+      <section className="delete-accounts">
+        <div className="delete-accounts-wrap">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <h3>Business Account</h3>
+            <p>Delete all transactions data from Business Account</p>
+          </div>
+          <button
+            style={{
+              border: "none",
+              color: "white",
+              background: "red",
+              marginLeft: "5px",
+              cursor: "pointer",
+              fontSize: "14px"
+            }}
+            title="Delete"
+            onClick={deleteItemTwo}
+          >
+            Delete
+          </button>
+        </div>
+      </section>
     </div>
   );
 }

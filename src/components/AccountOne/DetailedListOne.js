e991a5b4cac4ee3c245a6c9bf19df5474ecbb5f2
Segun <shegunolanitori@gmail.com>
2020-03-27 00:52:09 +0100

    Created Percentages from newly added Categories

--- src/components/AccountOne/DetailedListOne.js
+++ src/components/AccountOne/DetailedListOne.js
@@ -3,15 +3,128 @@ import { AccountOneContext } from "../../Contexts/AccountOne";
 import Commas from "../../Commas";
 
 export default function DetailedListOne() {
-  const { valueTransactionOne, AccountOneBalance } = useContext(
-    AccountOneContext
-  );
+  const {
+    valueTransactionOne,
+    AccountOneBalance,
+    expensesOneArray,
+    incomeOneTotal,
+    ExpensesOneTotal
+  } = useContext(AccountOneContext);
 
   const [TransactionOne] = valueTransactionOne;
 
-  // List according to time of transaction , no longer needed with the new code. see AccountOne context for old code
+  // List according to time of transaction , no longer needed.
   // const arranged = TransactionOne.sort((a, b) => new Date(a.time) - new Date(b.time));
 
+  // Percentage Calculation for each Category
+
+  function Education() {
+    const Education = expensesOneArray.filter(
+      one => one.Category === "Education"
+    );
+    let TotalEducation = Education.reduce((a, b) => a + b.amount, 0);
+    // console.log(TotalEducation);
+
+    let educationPercent = (TotalEducation / incomeOneTotal) * 100;
+
+    return Math.round(educationPercent);
+  }
+  //  Education();
+
+  console.log("Education Takes " + Education() + "%");
+
+  function Feeding() {
+    const Feeding = expensesOneArray.filter(one => one.Category === "Feeding");
+    let TotalFeeding = Feeding.reduce((a, b) => a + b.amount, 0);
+    // console.log(TotalFeeding);
+
+    let feedingPercent = (TotalFeeding / incomeOneTotal) * 100;
+    return Math.round(feedingPercent);
+  }
+
+  // Feeding()
+
+  console.log("Feeding Takes " + Feeding() + "%");
+
+  function Telephone() {
+    const Telephone = expensesOneArray.filter(
+      one => one.Category === "Telephone & Internet"
+    );
+    let TotalTelephone = Telephone.reduce((a, b) => a + b.amount, 0);
+    // console.log(TotalTelephone);
+
+    let telephonePercent = (TotalTelephone / incomeOneTotal) * 100;
+    return Math.round(telephonePercent);
+  }
+
+  // Telephone()
+
+  console.log("Telephone Takes " + Telephone() + "%");
+
+  function HealthCare() {
+    const HealthCare = expensesOneArray.filter(
+      one => one.Category === "HealthCare"
+    );
+    let TotalHealthCare = HealthCare.reduce((a, b) => a + b.amount, 0);
+    // console.log(TotalHealthCare);
+
+    let HealthCarePercent = (TotalHealthCare / incomeOneTotal) * 100;
+    return Math.round(HealthCarePercent);
+  }
+
+  // HealthCare()
+
+  console.log("HealthCare Takes " + HealthCare() + "%");
+
+  function Transportation() {
+    const Transportation = expensesOneArray.filter(
+      one => one.Category === "Transportation"
+    );
+    let TotalTransportation = Transportation.reduce((a, b) => a + b.amount, 0);
+    // console.log(TotalTransportation);
+
+    let TransportationPercent = (TotalTransportation / incomeOneTotal) * 100;
+    return Math.round(TransportationPercent);
+  }
+
+  // Transportation()
+
+  console.log("Transportation Takes " + Transportation() + "%");
+
+  let total =
+    Transportation() + HealthCare() + Education() + Feeding() + Telephone();
+  console.log("total " + total);
+
+  // Quiff Maffs to sum the "Other" to the remaining left out percentage
+  let rest = 100 - total;
+
+  function Other() {
+    const Other = expensesOneArray.filter(one => one.Category === "Other");
+    let TotalOther = Other.reduce((a, b) => a + b.amount, 0);
+    // console.log(TotalOther);
+
+    let OtherPercent = (TotalOther / incomeOneTotal) * 100;
+
+    console.log("Other " + Math.round(OtherPercent));
+    let remain = rest - OtherPercent;
+    return remain + Math.round(OtherPercent);
+  }
+
+  console.log("Other " + Math.round(Other()) + "%");
+
+  // Percentage of All Expenses from Income
+
+  function percentage() {
+    const div = ExpensesOneTotal / incomeOneTotal;
+
+    const percent = div * 100;
+    // console.log(percent);
+
+    return Math.round(percent);
+
+    // .toFixed(1)
+  }
+  percentage();
   return (
     <div className="detailed-list">
       <div className="detailed-list-head">

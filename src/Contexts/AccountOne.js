11bb1a31ff832ff913056a28e4ec21f0e1cedea5
Segun <shegunolanitori@gmail.com>
2020-03-27 00:11:49 +0100

    Found Total directly from the Array - Previously mapped the amount before calculating total

--- src/Contexts/AccountOne.js
+++ src/Contexts/AccountOne.js
@@ -3,23 +3,16 @@ import React, { createContext, useState, useEffect } from "react";
 export const AccountOneContext = createContext();
 
 export function AccountOneProvider(props) {
-  //Refactored code, Transaction for the first account is now in a single state. Scroll down to see the old code.
+  //Transaction State
   const [TransactionOne, setTransactionOne] = useState(StoreTransactionOne);
 
   //Filtered Expense and income
   const expensesOneArray = TransactionOne.filter(t => t.type === "Expense");
   const incomeOneArray = TransactionOne.filter(t => t.type === "Income");
 
-  // Map through amount
-  const expensesOneAmount = expensesOneArray.map(expenses => expenses.amount);
-  const incomeOneAmount = incomeOneArray.map(income => income.amount);
-
   // find total
-  const ExpensesOneTotal = expensesOneAmount.reduce(
-    (acc, item) => acc + item,
-    0
-  );
-  const incomeOneTotal = incomeOneAmount.reduce((acc, item) => acc + item, 0);
+  const ExpensesOneTotal = expensesOneArray.reduce((a, b) => a + b.amount, 0);
+  const incomeOneTotal = incomeOneArray.reduce((a, b) => a + b.amount, 0);
 
   // finds the balance
 
@@ -70,50 +63,3 @@ export function AccountOneProvider(props) {
     </AccountOneContext.Provider>
   );
 }
-
-//OLD CODE
-
-// const [IncomeOne, setIncomeOne] = useState(StoreIncomeOne);
-// const [ExpensesOne, setExpensesOne] = useState(StoreExpensesOne);
-
-//Mapped through the amounts
-// const IncomeOneAmounts = IncomeOne.map(incomeOne => incomeOne.amount);
-
-// const ExpensesOneAmounts = ExpensesOne.map(expensesOne => expensesOne.amount);
-
-//found the total
-
-// const incomeOneTotal = IncomeOneAmounts.reduce((acc, item) => acc + item, 0);
-//console.log(incomeOneTotal);
-
-// const ExpensesOneTotal = ExpensesOneAmounts.reduce(
-//   (acc, item) => acc + item,
-//   0
-// );
-
-//console.log(ExpensesOneTotal);
-
-// finds the balance
-
-// const AccountOneBalance = incomeOneTotal - ExpensesOneTotal;
-//console.log(AccountOneBalance);
-
-//PERSIST INPUTS TO LOCAL STORAGE
-
-// useEffect(() => {
-//   localStorage.setItem("incomeOne", JSON.stringify(IncomeOne));
-// }, [IncomeOne]);
-
-// function StoreIncomeOne() {
-//   const SavedIncomeOne = JSON.parse(localStorage.getItem("incomeOne"));
-//   return SavedIncomeOne || [];
-// }
-
-// useEffect(() => {
-//   localStorage.setItem("expensesOne", JSON.stringify(ExpensesOne));
-// }, [ExpensesOne]);
-
-// function StoreExpensesOne() {
-//   const SavedExpensesOne = JSON.parse(localStorage.getItem("expensesOne"));
-//   return SavedExpensesOne || [];
-// }

96ce1d52738572385af96497677b2e6f07c76b24
Segun <shegunolanitori@gmail.com>
2020-04-07 17:39:23 +0100

    trying to push to heroku

--- src/Contexts/AccountOne.js
+++ src/Contexts/AccountOne.js
@@ -12,10 +12,7 @@ export function AccountOneProvider(props) {
  async function getTransactions() {
   try {
     const res = await axios.get('/api/personal');
-    console.log(res.data);
     const data = res.data.data
-    console.log(data);
-    
     setTransactionOne(data)
   } catch (err) {
     console.log('internal error');

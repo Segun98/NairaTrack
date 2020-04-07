1bc555927f71f1dd6247e940ced7ffee815e88b4
Segun <shegunolanitori@gmail.com>
2020-04-07 18:01:37 +0100

    trying to push to heroku

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8080
 connectDB()
 
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
 app.use('/api/personal', accountone)

fb3bb060d5c0d392ae350d98bea70c702d50d3af
Segun <shegunolanitori@gmail.com>
2020-04-07 14:03:18 +0100

    trying to deploy to heroku

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const accountone = require('./routes/accountone')
 const accounttwo = require('./routes/accounttwo')
 
 dotenv.config()
- 
+
 const PORT = process.env.PORT || 8080
 
 connectDB()
@@ -24,14 +24,12 @@ app.use(cors())
 app.use('/api/personal', accountone)
 app.use('/api/business', accounttwo)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../build'));
+app.use(express.static('../build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
-    })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
+})
 
-}
 
 
 
